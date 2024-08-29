03cc20bb1c13f8c03f4fcbf66ff9af58816fdd53
Mahijeet Reddy <mahijeetreddy@gmail.com>
2024-08-28 20:21:19 -0700

    Updates

---
+++ models/Task.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    dueDate: {
+        type: Date
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        default: 'Medium'
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'In Progress', 'Completed'],
+        default: 'Pending'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
